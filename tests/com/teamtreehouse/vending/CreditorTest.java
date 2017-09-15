70d0605cf7c0b00ed21dfe4fd7356af7503bf719
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-09-15 14:15:51 -0500

    first commit of canary test

---
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,18 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by moon on 9/14/17.
+ */
+public class CreditorTest {
+    @Test
+    public void refund() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void example() throws Exception {
+    }
+}
