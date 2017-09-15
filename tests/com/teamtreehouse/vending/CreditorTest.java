d63b356a99d586b36fbdec0af9dea167bf14aefb
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-09-15 14:50:33 -0500

    add refundingReturnsAllAvailableFunds test add refundingResetsAvailableFundsToZero test add addingFundsIncrementsAvailableFunds test

--- tests/com/teamtreehouse/vending/CreditorTest.java
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -8,11 +8,37 @@ import static org.junit.Assert.*;
  */
 public class CreditorTest {
     @Test
-    public void refund() throws Exception {
-        fail();
+    public void refundingReturnsAllAvailableFunds() throws Exception {
+        Creditor creditor = new Creditor();
+
+        creditor.addFunds(10);
+        int refund = creditor.refund();
+
+        assertEquals(10, refund);
     }
 
     @Test
-    public void example() throws Exception {
+    public void refundingResetsAvailableFundsToZero() throws Exception {
+        Creditor creditor = new Creditor();
+
+        creditor.addFunds(10);
+        int refund = creditor.refund();
+
+        assertEquals(0, creditor.getAvailableFunds());
     }
+
+    @Test
+    public void addingFundsIncrementsAvailableFunds() throws Exception {
+        // Arrange objects so they're ready for testing
+        Creditor creditor = new Creditor();
+
+        // Act perform the object under test
+        creditor.addFunds(25);
+        creditor.addFunds(25);
+
+        // Assert that the expected == actual
+        // argument order is expected, then actual
+        assertEquals(50, creditor.getAvailableFunds());
+    }
+
 }
